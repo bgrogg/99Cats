396d4e37b7948cd2ea0f1b7abf509b461c960d88
App Academy Student <student@appacademy.io>
2016-12-06 17:00:05 -0800

    Add associations

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,7 +1,12 @@
 class Cat < ActiveRecord::Base
   validates :birth_date, :color, :name, :sex, :description, presence: true
   validates :color, inclusion: ["brown", "pink", "blue"]
-  validates :sex, inclusion: ["F", "M"] 
+  validates :sex, inclusion: ["F", "M"]
+
+  has_many :requests
+    primary_key: :id,
+    foreign_key: :cat_id,
+    class_name: :CatRentalRequest, dependent: :destroy
 
   def age
     ((Date.today - birth_date)/365).to_i
